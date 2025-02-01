1f9f5fa8d41c434f4089c855921728e3f26fd5c5
Utsav Prasad Sah <utsavpdsah75@gmail.com>
2025-02-01 12:44:12 +0545

    modifiy

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,16 +1,1 @@
-
-void pizza();
-void burger();
-void sandwich();
-void french_fries();
-void ice_cream();
-int main()
-{
-    int choice;
-    printf("Enter your choice\n");
-    printf("1. Pizza\n");
-    printf("2. Burger\n");
-    printf("3. Sandwich\n");
-    printf("4. French Fries\n");
-    printf("5. Ice Cream\n");
-    scanf
+,
