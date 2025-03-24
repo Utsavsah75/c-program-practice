645a963938db8c460254860b160165fbd4d2c982
Utsav Prasad Sah <120491657+Utsavsah75@users.noreply.github.com>
2025-03-24 17:31:28 +0545

    Update str_comp-concate.c

--- 9.String/str_comp-concate.c
+++ 9.String/str_comp-concate.c
@@ -1,5 +1,5 @@
+// program using string compare and concatenate witout using library function 
 #include <stdio.h>
-
 int main() {
     char str1[100], str2[100], concat[200];
     int i, j, isEqual = 1;
@@ -18,7 +18,6 @@ int main() {
             break;
         }
     }
-
     if (isEqual)
         printf("The two strings are equal.\n");
     else
@@ -28,15 +27,11 @@ int main() {
     for (i = 0; str1[i] != '\0'; i++) {
         concat[i] = str1[i];
     }
-
     for (j = 0; str2[j] != '\0'; j++) {
         concat[i + j] = str2[j];
     }
-
     concat[i + j] = '\0';  // Null-terminate the concatenated string
-
     printf("Concatenated String: %s\n", concat);
-
     return 0;
 }
 /*
