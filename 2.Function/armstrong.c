3076701b24a164c46db3cb2da19d56c5ba76389f
Utsav Prasad Sah <utsavpdsah75@gmail.com>
2025-03-30 07:49:52 +0545

    armstrong

--- 2.Function/armstrong.c
+++ 2.Function/armstrong.c
@@ -1,33 +1,37 @@
-// WAP to check whether a number is Armstrong number or not using function.
-#include <math.h>
 #include <stdio.h>
-int countDigits(int num) {
-  int count = 0;
-  while (num > 0) {
-    count++;
-    num /= 10;
-  }
-  return count;
-}
-int isArmstrong(int num) {
-  int num1=num,sum=0,n,digit;
-  n = countDigits(num);
-  while (num > 0) {
-    digit = num % 10;
-    sum += pow(digit, n);
-    num /= 10;
-  }
-  return (sum == num1);
-}
-int main() {
-  int num;
-  printf("Enter a number: ");
-  scanf("%d", &num);
+#include <math.h>
 
-  if (isArmstrong(num)) {
-    printf("%d is an Armstrong number.\n", num);
-  } else {
-    printf("%d is not an Armstrong number.\n", num);
-  }
-  return 0;
+// Function to count the number of digits in a number
+int countDigits(int num) {
+    int count = 0;
+    while (num > 0) {
+        count++;
+        num /= 10;
+    }
+    return count;
+}
+
+// Function to check if a number is an Armstrong number
+int isArmstrong(int num) {
+    int num1 = num, sum = 0, n, digit;
+    n = countDigits(num);
+    while (num1 > 0) {
+        digit = num1 % 10;
+        sum += pow(digit, n);
+        num1 /= 10;
+    }
+    return (sum == num);
+}
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (isArmstrong(num)) {
+        printf("%d is an Armstrong number.\n", num);
+    } else {
+        printf("%d is not an Armstrong number.\n", num);
+    }
+    return 0;
 }
